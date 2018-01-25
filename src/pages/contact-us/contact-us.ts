0d996e5431ce3f0faa0797be158bbfbc2a081299
danhab99 <dan.habot@gmail.com>
2018-01-25 14:08:10 +0200

    Finished contact us page

--- src/pages/contact-us/contact-us.ts
+++ src/pages/contact-us/contact-us.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { ElementRef, ViewChild } from '@angular/core';
 
 /**
  * Generated class for the ContactUsPage page.
@@ -14,10 +16,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'contact-us.html',
 })
 export class ContactUsPage {
-//  @ViewChild('commentField');
-  commentField: ElementRef;
+  @ViewChild('commentField') commentField: ElementRef;
+  @ViewChild('commentLabel') commentLabel: ElementRef;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: Http
+  ) {
 
   }
 
@@ -25,8 +31,14 @@ export class ContactUsPage {
     console.log('ionViewDidLoad ContactUsPage');
   }
 
-  submit(){
-    
+  submit() {
+    let m = this.commentField.nativeElement.value;
+    let data = {
+      "Name": "Debug app",
+      "Email": "dan.habot@gmail.com",
+      "Message": m
+    };
+    this.http.post("http://startups.cdi-negev.com/send-email.php", data).subscribe(d => console.log(d));
   }
 
 }
